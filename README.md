563f8c5c228e82d4b387dd991b6efe8102a02934
Chris M <webmaster@magnaws.com>
2018-07-09 06:41:23 -0400

    Set up a basic "front page" for the BAOBAB test harness. Added a rudimentary README.

---
+++ README.md
@@ -0,0 +1,13 @@
+\page BAOBAB BAOBAB
+
+![BASALT](images/BASALT.png)
+
+BAOBAB
+======
+
+LICENSE
+=======
+
+![Little Green Viper Software Development LLC](images/viper.png)
+© Copyright 2018, [Little Green Viper Software Development LLC](https://littlegreenviper.com).
+This code is ENTIRELY proprietary and not to be reused, unless under specific, written license from [Little Green Viper Software Development LLC](https://littlegreenviper.com).
--- index.php
+++ index.php
@@ -11,95 +11,25 @@
 
     Little Green Viper Software Development: https://littlegreenviper.com
 */
-    define('LGV_CONFIG_CATCHER', '1');
-    require_once(dirname(__FILE__).'/config/s_config.class.php');
-    require_once(dirname(__FILE__).'/php/testing_functions.php');
+$config_file_path = dirname(__FILE__).'/config/s_config.class.php';
 
-    date_default_timezone_set ( 'UTC' );
-    global $test_name_array;
-    
-    function iterate_directory( $in_dirname
-                                ) {
-        global $test_name_array;
-        $iterator = new DirectoryIterator($in_dirname);
+if ( !defined('LGV_CONFIG_CATCHER') ) {
+    define('LGV_CONFIG_CATCHER', 1);
+}
 
-        foreach ($iterator as $fileinfo) {
-            if (('.' === substr($fileinfo->getBasename(), 0, 1))) {
-                continue;
-            } elseif (('php' === $fileinfo->getExtension()) && ('index.php' != $fileinfo->getBasename())) {
-                $test_name_array[] = basename(dirname($fileinfo->getPathname())).'/'.substr($fileinfo->getBasename(), 0, -4);
-            } elseif (!$fileinfo->isDot() && $fileinfo->isDir()) {
-                iterate_directory($fileinfo->getPathname());
-            }
-        }
-    }
-    
-    $test_name_array = [];
-    
-    iterate_directory(dirname(__FILE__).'/test-scripts/');    
-    
-    if (isset($test_name_array) && is_array($test_name_array) && count($test_name_array)) {
-        sort($test_name_array);
-        $test_name_array = "Array('".implode("','", $test_name_array)."')";
-    } else {
-        $test_name_array = "Array('')";
-    }
-    
+require_once($config_file_path);
+
 ?><!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>BAOBAB Test Harness</title>
+        <title>BASALT</title>
         <link rel="shortcut icon" href="icon.png" type="image/png" />
         <style>
             *{margin:0;padding:0}
             body {
                 font-family: Arial, San-serif;
-                text-align:center;
                 }
-            
-            div.timing_report,
-            div.test_report {
-                margin-top: 1em;
-                margin-bottom: 1em;
-                margin-left: 1em;
-            }
-            
-            div.timing_report {
-                font-style: italic;
-                color: #930;
-            }
-            
-            div.test_report {
-                border-radius: 0.5em;
-                padding: 0.25em;
-                border: 1px dashed black;
-            }
-            
-            div.test_report.good_report {
-                background-color: #dfd;
-            }
-            
-            div.test_report.bad_report {
-                background-color: #fdd;
-            }
-            
-            div.closed,
-            div.open,
-            div.inner_closed {
-                display: table;
-                margin:auto;
-            }
-            
-            div.inner_closed,
-            div.inner_open {
-                margin-top:0.25em;
-                margin-bottom: 0.25em;
-            }
-            
-            div.indent_1 {
-                margin-left:1em;
-            }
             
             div.main_div {
                 margin-top:0.25em;
@@ -108,146 +38,68 @@
                 padding: 0.5em;
             }
             
-            div.inner_div {
-                margin-top:0.25em;
-                padding: 0.25em;
-            }
-            
             .explain {
                 font-style: italic;
-                display:block;
-                margin:0.5em;
-                text-align:left;
             }
             
             h1.header {
-                font-size: x-large;
-            }
-            
-            h2.header {
                 font-size: large;
+                margin-top: 1em;
+                text-align:center;
             }
             
-            div.closed,
-            div.open {
-                margin-top:0.5em;
-                border:1px solid #555;
-                border-radius:0.25em;
-                padding: 0.25em;
-                min-width:30em;
-            }
-            
-            div.open div.container {
-                margin:auto;
-                display: table;
-                text-align:left;
-            }
-            
-            div.closed div.container {
-                display: none;
-            }
-            
-            h3.inner_header {
-                font-size: medium;
-                display:table;
-                margin:auto;
-            }
-            
-            div.inner_open {
-                border:1px dashed #555;
-                border-radius:0.25em;
-                padding: 0.25em;
-            }
-            
-            div.inner_closed h2.inner_header {
-            }
-            
-            div.inner_open h3.inner_header {
-                display:block;
-                text-align:left;
-            }
-            
-            div.inner_open div.inner_container {
-                display: table;
-                margin:auto;
-            }
-            
-            div.inner_closed div.inner_container {
-                display: none;
-            }
-            
-            div.test-wrapper {
-                display: table;
-                margin:auto;
-                padding: 0.25em;
-                margin-top:1em;
-                min-width:30em;
-            }
-            
-            div.collection_wrapper {
-            }
-            
-            div#tests-wrapped-up {
-                margin:auto;
-            }
         </style>
-        <script type="text/javascript" src="js/ajaxLoader.js"></script>
-        <script type="text/javascript" src="js/runTests.js"></script>
-        <script type="text/javascript">
-            function toggle_main_state(in_id) {
-                var item = document.getElementById(in_id);
-                
-                if ( item.className == 'closed' ) {
-                    item.className = 'open';
-                } else {
-                    item.className = 'closed';
-                };
-            };
-            
-            function toggle_inner_state(in_id) {
-                var item = document.getElementById(in_id);
-                
-                if ( item.className == 'inner_closed' ) {
-                    item.className = 'inner_open';
-                } else {
-                    item.className = 'inner_closed';
-                };
-                
-            };
-            
-            function expose_tests() {
-                var item = document.getElementById('throbber-container');
-                
-                if (item) {
-                    item.style="display:none";
-                };
-                
-                var item = document.getElementById('tests-wrapped-up');
-                
-                if (item) {
-                    item.style="display:block";
-                };
-            };
-        </script>
     </head>
     <body>
-        <h1 style="text-align:center">BAOBAB SERVER TESTS</h1>
+        <h1 style="text-align:center">BAOBAB APPLICATION SERVER</h1>
         <div style="text-align:center;padding:1em;">
-            <div id="throbber-container" style="text-align:center">
-                <h3 id="progress-report" style="margin-top:1em"></h3>
-                <img src="images/throbber.gif" alt="throbber" style="position:absolute;width:190px;top:50%;left:50%;margin-top:-95px;margin-left:-95px" />
-                <img src="icon.png" alt="icon" style="position:absolute;width:128px;top:50%;left:50%;margin-top:-64px;margin-left:-64px" />
-            </div>
             <?php
-            $start_time = microtime(true);
+                if ( !defined('LGV_BASALT_CATCHER') ) {
+                    define('LGV_BASALT_CATCHER', 1);
+                }
+    
+                require_once(CO_Config::main_class_dir()."/co_basalt.class.php");
             ?>
-            <div id="tests-wrapped-up" style="display:none">
-                <img src="icon.png" style="display:block;margin:auto;width:80px" alt="" />
-                <div id="tests-displayed"></div>
+            <img src="icon.png" style="display:block;margin:auto;width:80px" alt="BAOBAB Logo" />
+            <h1 class="header">MAIN ENVIRONMENT SETUP</h1>
+            <div style="text-align:left;margin:auto;display:table">
+                <div class="main_div container">
+                    <?php
+                        echo("<div style=\"margin:auto;text-align:center;display:table\">");
+                        echo("<h2>File/Folder Locations</h2>");
+                        echo("<pre style=\"margin:auto;text-align:left;display:table\">");
+                        echo("<strong>BASALT Version</strong>..........".__BASALT_VERSION__."\n");
+                        echo("<strong>ANDISOL Version</strong>.........".__ANDISOL_VERSION__."\n");
+                        echo("<strong>COBRA Version</strong>...........".__COBRA_VERSION__."\n");
+                        echo("<strong>CHAMELEON Version</strong>.......".__CHAMELEON_VERSION__."\n");
+                        echo("<strong>BADGER Version</strong>..........".__BADGER_VERSION__."\n");
+                        echo("<strong>BASALT Base dir</strong>.........".CO_Config::base_dir()."\n");
+                        echo("<strong>ANDISOL Base dir</strong>........".CO_Config::andisol_base_dir()."\n");
+                        echo("<strong>COBRA Base dir</strong>..........".CO_Config::cobra_base_dir()."\n");
+                        echo("<strong>CHAMELEON Base dir</strong>......".CO_Config::chameleon_base_dir()."\n");
+                        echo("<strong>BADGER Base dir</strong>.........".CO_Config::badger_base_dir()."\n");
+                        foreach (CO_Config::db_classes_extension_class_dir() as $dir) {
+                            echo("<strong>Extension classes dir:</strong>..$dir\n");
+                        }
+                        echo("</pre></div>");
+                    ?>
+                    <div class="main_div">
+                        <h2 style="text-align:center">Instructions</h2>
+                        <p class="explain">In order to run these tests, you should set up two (2) blank databases. They can both be the same DB, but that is not the advised configuration for Badger.</p>
+                        <p class="explain">The first (main) database should be called "<?php echo(CO_Config::$data_db_name) ?>", and the second (security) database should be called "<?php echo(CO_Config::$sec_db_name) ?>".</p>
+                        <p class="explain">The main database should be have a full rights login named "<?php echo(CO_Config::$data_db_login) ?>", with a password of "<?php echo(CO_Config::$data_db_password) ?>".</p>
+                        <p class="explain">The security database should have a full rights login named "<?php echo(CO_Config::$sec_db_login) ?>", with a password of "<?php echo(CO_Config::$sec_db_password) ?>".</p>
+                        <p class="explain" style="font-weight:bold;color:red;font-style:italic">This test will wipe out the tables, and set up pre-initialized tables, so it goes without saying that these should be databases (and users) reserved for testing only.</p>
+                    </div>
+                </div>
             </div>
-            <script type="text/javascript">
-                runTests(<?php echo($test_name_array) ?>);
-            </script>
+            <h3 style="margin-top:1em"><a href="test.php">BAOBAB TESTS</a></h3>
+            <h3 style="margin-top:1em"><a href="basalt/test/runTests.php">BASALT TESTS</a></h3>
+            <h3 style="margin-top:1em"><a href="basalt/andisol/test/">ANDISOL TESTS</a></h3>
+            <h3 style="margin-top:1em"><a href="basalt/andisol/cobra/test/">COBRA TESTS</a></h3>
+            <h3 style="margin-top:1em"><a href="basalt/andisol/cobra/chameleon/test/">CHAMELEON TESTS</a></h3>
+            <h3 style="margin-top:1em"><a href="basalt/andisol/cobra/chameleon/badger/test/">BADGER TESTS</a></h3>
         </div>
     </body>
 </html>
+
