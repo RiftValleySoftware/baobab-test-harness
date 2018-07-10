ae9d180f19feafbd46fc95630b1c5c632225a139
Chris M <webmaster@magnaws.com>
2018-07-10 17:06:34 -0400

    Added tests for accessing via the login ID.

--- test-scripts/people/02-get_selected_people_tests.php
+++ test-scripts/people/02-get_selected_people_tests.php
@@ -20,7 +20,7 @@ baobab_run_tests(26, 'BASIC SELECTED PEOPLE TESTS', 'Access Users and Logins Spe
 // -------------------------- DEFINITIONS AND TESTS -----------------------------------
 
 function basalt_test_define_0026() {
-    basalt_run_single_direct_test(26, 'Access Users and Logins By Numerical ID (JSON)', 'GET tests for people and logins; with and without various logins.', 'people_tests');
+    basalt_run_single_direct_test(26, 'Access A Single User By Numerical ID (JSON)', 'GET tests for users, where we access one single user, by its numerical ID.', 'people_tests');
 }
 
 function basalt_test_0026($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
@@ -305,9 +305,10 @@ function basalt_test_0026($in_login = NULL, $in_hashed_password = NULL, $in_pass
     call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
 }
 
+// --------------------
 
 function basalt_test_define_0027() {
-    basalt_run_single_direct_test(27, 'Access Users and Logins By Numerical ID (JSON)', 'GET tests for people and logins; with and without various logins.', 'people_tests');
+    basalt_run_single_direct_test(27, 'Access A Single User By Numerical ID (XML)', 'GET tests for users, where we access one single user, by its numerical ID.', 'people_tests');
 }
 
 function basalt_test_0027($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
@@ -576,6 +577,222 @@ function basalt_test_0027($in_login = NULL, $in_hashed_password = NULL, $in_pass
     $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=admin&password='.CO_Config::god_mode_password(), NULL, NULL, $result_code);
     $expected_result_code = 200;
     $expected_result = get_xml_header('people').'<people><value sequence_index="0"><id>1725</id><name>MDAdmin</name><lang>en</lang><coords>39.285800,-76.613100</coords><write_token>7</write_token><last_access>1970-01-02 00:00:00</last_access><writeable>1</writeable><latitude>39.2858</latitude><longitude>-76.6131</longitude><middle_name>TAG-2-TEST-PEOPLE</middle_name><associated_login><id>7</id><name>Maryland Login</name><lang>en</lang><login_id>MDAdmin</login_id><read_token>7</read_token><write_token>7</write_token><last_access>1970-01-02 00:00:00</last_access><writeable>1</writeable><user_object_id>1725</user_object_id><security_tokens><value sequence_index="0">7</value></security_tokens></associated_login></value></people></people>';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+}
+
+// --------------------
+
+function basalt_test_define_0028() {
+    basalt_run_single_direct_test(28, 'Access A Single User By Login ID (JSON)', 'GET tests for users, where we access one single user, by its numerical or string login ID.', 'people_tests');
+}
+
+function basalt_test_0028($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+    $title = 'People Test 28A: Access A Single User (with an associated login) by Numerical ID (Not Logged In)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/7';
+    $data = NULL;
+    $api_key = NULL;
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    $title = 'People Test 28B: Access A Single User (with an associated login) by String ID (Not Logged In)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/MDAdmin';
+    $data = NULL;
+    $api_key = NULL;
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    $title = 'People Test 28C: Access A Single User (with an associated login) by Numerical ID (Logged In as a Normal User that Cannot See the Login)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/7';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MeLeet&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    $title = 'People Test 28D: Access A Single User (with an associated login) by String ID (Logged In as a Normal User that Cannot See the Login)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/MDAdmin';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MeLeet&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    $title = 'People Test 28E: Access A Single User (with an associated login) by Numerical ID (Logged In as a Manager that Cannot See the Login)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/7';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=PHB&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    $title = 'People Test 28F: Access A Single User (with an associated login) by String ID (Logged In as a Manager that Cannot See the Login)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/MDAdmin';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=PHB&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    $title = 'People Test 28G: Access A Single User (with an associated login) by Numerical ID (Logged In as a Manager that Can See the Login)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/7';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MainAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[{"id":1725,"name":"MDAdmin","lang":"en","coords":"39.285800,-76.613100"}]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+    
+    $title = 'People Test 28H: Access A Single User (with an associated login) by String ID (Logged In as a Manager that Can See the Login)';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/MDAdmin';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MainAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[{"id":1725,"name":"MDAdmin","lang":"en","coords":"39.285800,-76.613100"}]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+    
+    $title = 'People Test 28I: Access A Single User (with an associated login) by Numerical ID (Logged In as "God"). Also, show lots of detail.';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/7?login_user';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=admin&password='.CO_Config::god_mode_password(), NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[{"id":1725,"name":"MDAdmin","lang":"en","coords":"39.285800,-76.613100","read_token":0,"write_token":7,"last_access":"1970-01-02 00:00:00","writeable":true,"latitude":39.2858,"longitude":-76.6131,"middle_name":"TAG-2-TEST-PEOPLE","associated_login":{"id":7,"name":"Maryland Login","lang":"en","login_id":"MDAdmin","read_token":7,"write_token":7,"last_access":"1970-01-02 00:00:00","writeable":true,"user_object_id":1725,"security_tokens":[7]}}]}}';
+    $result_code = '';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+    
+    $st1 = microtime(true);
+    $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+    
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result);
+    }
+    
+    call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+    
+    $title = 'People Test 28J: Access A Single User (with an associated login) by String ID (Logged In as "God"). Also, show lots of detail.';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/people/people/login_ids/MDAdmin?login_user';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=admin&password='.CO_Config::god_mode_password(), NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = '{"people":{"people":[{"id":1725,"name":"MDAdmin","lang":"en","coords":"39.285800,-76.613100","read_token":0,"write_token":7,"last_access":"1970-01-02 00:00:00","writeable":true,"latitude":39.2858,"longitude":-76.6131,"middle_name":"TAG-2-TEST-PEOPLE","associated_login":{"id":7,"name":"Maryland Login","lang":"en","login_id":"MDAdmin","read_token":7,"write_token":7,"last_access":"1970-01-02 00:00:00","writeable":true,"user_object_id":1725,"security_tokens":[7]}}]}}';
     $result_code = '';
     
     test_header($title, $method, $uri, $expected_result_code);
