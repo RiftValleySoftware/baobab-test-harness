c6e231a7fce27efeebc64cc243583758feef65e6
Chris M <webmaster@magnaws.com>
2018-07-11 07:36:58 -0400

    Added the serverinfo response. Continuing work on the people/login query tests.

--- test-scripts/baseline/05-token_baseline_tests.php
+++ test-scripts/baseline/05-token_baseline_tests.php
@@ -15,12 +15,15 @@
 
 require_once(dirname(dirname(dirname(__FILE__))).'/php/run_baobab_tests.php');
 
-baobab_run_tests(16, 'BASELINE TOKEN MANAGEMENT', 'We test to make sure that we can see whatever tokens we have available, and create new ones, if allowed.');
+define('LGV_BASALT_CATCHER', 1);
+require_once(dirname(dirname(dirname(__FILE__))).'/basalt/main/co_basalt.class.php');
+
+baobab_run_tests(16, 'BASELINE TOKEN MANAGEMENT AND SERVERINFO', 'We test to make sure that we can see whatever tokens we have available, and create new ones, if allowed. We also check the serverinfo.');
 
 // -------------------------- DEFINITIONS AND TESTS -----------------------------------
 
 function basalt_test_define_0016() {
-    basalt_run_single_direct_test(16, 'Check Our Tokens (JSON)', 'Check tokens for various logins.', 'baseline_tests');
+    basalt_run_single_direct_test(16, 'Check Our Tokens and serverinfo (JSON)', 'Check tokens for various logins, as well as the serverinfo.', 'baseline_tests');
 }
 
 function basalt_test_0016($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
@@ -131,6 +134,84 @@ function basalt_test_0016($in_login = NULL, $in_hashed_password = NULL, $in_pass
             test_result_good($result_code, $result, $st1, $expected_result);
         }
     
+        $title = 'Baseline Test 16E: Log in as "God," and get the serverinfo.';
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/json/baseline/serverinfo';
+        $expected_result_code = 200;
+        $expected_result = '{"baseline":{"serverinfo":{"basalt_version":"'.__BASALT_VERSION__.'","andisol_version":"'.__ANDISOL_VERSION__.'","cobra_version":"'.__COBRA_VERSION__.'","chameleon_version":"'.__CHAMELEON_VERSION__.'","badger_version":"'.__BADGER_VERSION__.'","security_db_type":"'.CO_Config::$sec_db_type.'","data_db_type":"'.CO_Config::$data_db_type.'","min_pw_length":'.intval(CO_Config::$min_pw_len).',"regular_timeout_in_seconds":'.intval(CO_Config::$session_timeout_in_seconds).',"god_timeout_in_seconds":'.intval(CO_Config::$god_session_timeout_in_seconds).',"block_repeated_logins":'.(CO_Config::$block_logins_for_valid_api_key ? 'true' : 'false').',"block_differing_ip_address":'.(CO_Config::$api_key_includes_ip_address ? 'true' : 'false').',"ssl_requirement_level":'.intval(CO_Config::$ssl_requirement_level).'}}}';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
+        call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+    
+        $title = 'Baseline Test 16F: Try serverinfo with no login.';
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/json/baseline/serverinfo';
+        $expected_result_code = 401;
+        $expected_result = '';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
+        $title = 'Baseline Test 16G: Try serverinfo with regular login.';
+        $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MDAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/json/baseline/serverinfo';
+        $expected_result_code = 200;
+        $expected_result = '{"baseline":[]}';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
+        call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+    
+        $title = 'Baseline Test 16H: Try serverinfo with manager login.';
+        $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MainAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/json/baseline/serverinfo';
+        $expected_result_code = 200;
+        $expected_result = '{"baseline":[]}';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
         call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
     }
 }
@@ -138,7 +219,7 @@ function basalt_test_0016($in_login = NULL, $in_hashed_password = NULL, $in_pass
 // --------------------
 
 function basalt_test_define_0017() {
-    basalt_run_single_direct_test(17, 'Check Our Tokens (XML)', 'Check tokens for various logins.', 'baseline_tests');
+    basalt_run_single_direct_test(17, 'Check Our Tokens and serverinfo (XML)', 'Check tokens for various logins, as well as the serverinfo.', 'baseline_tests');
 }
 
 function basalt_test_0017($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
@@ -236,6 +317,105 @@ function basalt_test_0017($in_login = NULL, $in_hashed_password = NULL, $in_pass
     } else {
         $expected_result_code = 200;
         $expected_result = get_xml_header('baseline').'<tokens><value sequence_index="0">2</value><value sequence_index="1">1</value><value sequence_index="2">7</value><value sequence_index="3">8</value><value sequence_index="4">9</value><value sequence_index="5">10</value><value sequence_index="6">11</value><value sequence_index="7">12</value></tokens></baseline>';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
+        $title = 'Baseline Test 17E: Log in as "God," and get the serverinfo.';
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/xml/baseline/serverinfo';
+        $expected_result_code = 200;
+        $expected_result = get_xml_header('baseline').'<serverinfo>';
+        $expected_result .= '<basalt_version>'.__BASALT_VERSION__.'</basalt_version>';
+        $expected_result .= '<andisol_version>'.__ANDISOL_VERSION__.'</andisol_version>';
+        $expected_result .= '<cobra_version>'.__COBRA_VERSION__.'</cobra_version>';
+        $expected_result .= '<chameleon_version>'.__CHAMELEON_VERSION__.'</chameleon_version>';
+        $expected_result .= '<badger_version>'.__BADGER_VERSION__.'</badger_version>';
+        $expected_result .= '<security_db_type>'.CO_Config::$sec_db_type.'</security_db_type>';
+        $expected_result .= '<data_db_type>'.CO_Config::$data_db_type.'</data_db_type>';
+        $expected_result .= '<min_pw_length>'.intval(CO_Config::$min_pw_len).'</min_pw_length>';
+        $expected_result .= '<regular_timeout_in_seconds>'.intval(CO_Config::$session_timeout_in_seconds).'</regular_timeout_in_seconds>';
+        $expected_result .= '<god_timeout_in_seconds>'.intval(CO_Config::$god_session_timeout_in_seconds).'</god_timeout_in_seconds>';
+        if (CO_Config::$block_logins_for_valid_api_key) {
+            $expected_result .= '<block_repeated_logins>1</block_repeated_logins>';
+        }
+        if (CO_Config::$api_key_includes_ip_address) {
+            $expected_result .= '<block_differing_ip_address>1</block_differing_ip_address>';
+        }
+        if (intval(CO_Config::$ssl_requirement_level)) {
+            $expected_result .= '<ssl_requirement_level>'.intval(CO_Config::$ssl_requirement_level).'</ssl_requirement_level>';
+        }
+        
+        $expected_result .= '</serverinfo></baseline>';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
+        call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+    
+        $title = 'Baseline Test 17F: Try serverinfo with no login.';
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/xml/baseline/serverinfo';
+        $expected_result_code = 401;
+        $expected_result = '';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
+        $title = 'Baseline Test 17G: Try serverinfo with regular login.';
+        $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MDAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/xml/baseline/serverinfo';
+        $expected_result_code = 200;
+        $expected_result = get_xml_header('baseline').'</baseline>';
+        $result_code = '';
+
+        test_header($title, $method, $uri, $expected_result_code);
+
+        $st1 = microtime(true);
+        $result = call_REST_API($method, $uri, $data, $api_key, $result_code);
+
+        if ($result_code != $expected_result_code) {
+            test_result_bad($result_code, $result, $st1, $expected_result);
+        } else {
+            test_result_good($result_code, $result, $st1, $expected_result);
+        }
+    
+        call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+    
+        $title = 'Baseline Test 17H: Try serverinfo with manager login.';
+        $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MainAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+        $method = 'GET';
+        $uri = __SERVER_URI__.'/xml/baseline/serverinfo';
+        $expected_result_code = 200;
+        $expected_result = get_xml_header('baseline').'</baseline>';
         $result_code = '';
 
         test_header($title, $method, $uri, $expected_result_code);
