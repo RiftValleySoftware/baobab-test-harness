d1722af5c7f89ffc560462757f9e44f0b4ec65a9
Chris M <webmaster@magnaws.com>
2018-07-22 11:39:06 -0400

    Finished up the PUT tests for the things plugin (for now). Removed a file that was accidentally checked in at the wrong place.

--- test-scripts/things/04-things-put-tests.php
+++ test-scripts/things/04-things-put-tests.php
@@ -15,7 +15,7 @@
 
 require_once(dirname(dirname(dirname(__FILE__))).'/php/run_baobab_tests.php');
 
-baobab_run_tests(130, '130-: THINGS PUT TESTS', '');
+baobab_run_tests(130, '130-133: THINGS PUT TESTS', '');
 
 // -------------------------- DEFINITIONS AND TESTS -----------------------------------
 
@@ -236,6 +236,150 @@ function basalt_test_0131($in_login = NULL, $in_hashed_password = NULL, $in_pass
     } else {
         test_result_good($result_code, $result, $st1, $expected_result, false);
         log_entry(true, 131, $title);
+    }
+
+    call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+}
+
+// --------------------
+
+function basalt_test_define_0132() {
+    basalt_run_single_direct_test(132, 'BASIC PUT TESTS ON MULTIPLE OBJECTS (JSON)', '', 'things_tests_2');
+}
+
+function basalt_test_0132($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+    $title = 'Things Test 132A: Change the key, payload, name and description of three records.';
+    $method = 'PUT';
+    $uri = __SERVER_URI__.'/json/things/1732,1733,1734/?key=A+New+Simple+Text+Thing&payload=HELLO+WORLD&name=Howdy!&description=Simple+Text+Greeting&tag2=&tag3=&read_token=1&write_token=11';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MainAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = file_get_contents(dirname(__FILE__).'/04-things-put-tests-132A.json');
+    $result_code = '';
+
+    test_header($title, $method, $uri, $expected_result_code);
+
+    $st1 = microtime(true);
+    $result = clean_last_access_json(call_REST_API($method, $uri, $data, $api_key, $result_code));
+
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+        log_entry(false, 132, $title);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result, false);
+        log_entry(true, 132, $title);
+    }
+
+    $title = 'Things Test 132B: Look At Whichever One Comes Up With the New Key.';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/things/A+New+Simple+Text+Thing/?show_details';
+    $data = NULL;
+    $expected_result_code = 200;
+    $expected_result = '{"things":[{"id":1732,"name":"Howdy!","lang":"en","coords":"39.746352,-75.551615","read_token":1,"write_token":11,"last_access":"1970-01-02 00:00:00","writeable":true,"latitude":39.746352,"longitude":-75.551615,"payload_type":"text\/plain;base64","payload":"SEVMTE8gV09STEQ=","key":"A New Simple Text Thing","description":"Simple Text Greeting"}]}';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+
+    $st1 = microtime(true);
+    $result = clean_last_access_json(call_REST_API($method, $uri, $data, $api_key, $result_code));
+
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+        log_entry(false, 132, $title);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result, false);
+        log_entry(true, 132, $title);
+    }
+
+    $title = 'Things Test 132C: Look At All Of Them.';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/json/things/1732,1733,1734/?show_details';
+    $data = NULL;
+    $expected_result_code = 200;
+    $expected_result = '{"things":[{"id":1732,"name":"Howdy!","lang":"en","coords":"39.746352,-75.551615","read_token":1,"write_token":11,"last_access":"1970-01-02 00:00:00","writeable":true,"latitude":39.746352,"longitude":-75.551615,"payload_type":"text\/plain;base64","payload":"SEVMTE8gV09STEQ=","key":"A New Simple Text Thing","description":"Simple Text Greeting"},{"id":1733,"name":"Howdy!","lang":"en","coords":"39.648316,-77.719232","read_token":1,"write_token":11,"last_access":"1970-01-02 00:00:00","writeable":true,"latitude":39.648316,"longitude":-77.719232,"payload_type":"text\/plain;base64","payload":"SEVMTE8gV09STEQ=","key":"basalt-test-0171: Another World","description":"Simple Text Greeting"},{"id":1734,"name":"Howdy!","lang":"en","coords":"39.746352,-75.551615","read_token":1,"write_token":11,"last_access":"1970-01-02 00:00:00","writeable":true,"latitude":39.746352,"longitude":-75.551615,"payload_type":"text\/plain;base64","payload":"SEVMTE8gV09STEQ=","key":"basalt-test-0171: Top Shot","description":"Simple Text Greeting"}]}';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+
+    $st1 = microtime(true);
+    $result = clean_last_access_json(call_REST_API($method, $uri, $data, $api_key, $result_code));
+
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+        log_entry(false, 132, $title);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result, false);
+        log_entry(true, 132, $title);
+    }
+
+    call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
+}
+
+// --------------------
+
+function basalt_test_define_0133() {
+    basalt_run_single_direct_test(133, 'BASIC PUT TESTS ON MULTIPLE OBJECTS (XML)', '', 'things_tests_2');
+}
+
+function basalt_test_0133($in_login = NULL, $in_hashed_password = NULL, $in_password = NULL) {
+    $title = 'Things Test 133A: Change the key, payload, name and description of three records.';
+    $method = 'PUT';
+    $uri = __SERVER_URI__.'/xml/things/1732,1733,1734/?key=A+New+Simple+Text+Thing&payload=HELLO+WORLD&name=Howdy!&description=Simple+Text+Greeting&tag2=&tag3=&read_token=1&write_token=11';
+    $data = NULL;
+    $api_key = call_REST_API('GET', __SERVER_URI__.'/login?login_id=MainAdmin&password=CoreysGoryStory', NULL, NULL, $result_code);
+    $expected_result_code = 200;
+    $expected_result = get_xml_header('things').file_get_contents(dirname(__FILE__).'/04-things-put-tests-133A.xml');
+    $result_code = '';
+
+    test_header($title, $method, $uri, $expected_result_code);
+
+    $st1 = microtime(true);
+    $result = clean_last_access_xml(call_REST_API($method, $uri, $data, $api_key, $result_code));
+
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+        log_entry(false, 133, $title);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result, false);
+        log_entry(true, 133, $title);
+    }
+
+    $title = 'Things Test 133B: Look At Whichever One Comes Up With the New Key.';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/xml/things/A+New+Simple+Text+Thing/?show_details';
+    $data = NULL;
+    $expected_result_code = 200;
+    $expected_result = get_xml_header('things').'<value sequence_index="0"><id>1732</id><name>Howdy!</name><lang>en</lang><coords>39.746352,-75.551615</coords><read_token>1</read_token><write_token>11</write_token><last_access>1970-01-02 00:00:00</last_access><writeable>1</writeable><latitude>39.746352</latitude><longitude>-75.551615</longitude><payload_type>text/plain;base64</payload_type><payload>SEVMTE8gV09STEQ=</payload><key>A New Simple Text Thing</key><description>Simple Text Greeting</description></value></things>';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+
+    $st1 = microtime(true);
+    $result = clean_last_access_xml(call_REST_API($method, $uri, $data, $api_key, $result_code));
+
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+        log_entry(false, 133, $title);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result, false);
+        log_entry(true, 133, $title);
+    }
+
+    $title = 'Things Test 133C: Look At All Of Them.';
+    $method = 'GET';
+    $uri = __SERVER_URI__.'/xml/things/1732,1733,1734/?show_details';
+    $data = NULL;
+    $expected_result_code = 200;
+    $expected_result = get_xml_header('things').'<value sequence_index="0"><id>1732</id><name>Howdy!</name><lang>en</lang><coords>39.746352,-75.551615</coords><read_token>1</read_token><write_token>11</write_token><last_access>1970-01-02 00:00:00</last_access><writeable>1</writeable><latitude>39.746352</latitude><longitude>-75.551615</longitude><payload_type>text/plain;base64</payload_type><payload>SEVMTE8gV09STEQ=</payload><key>A New Simple Text Thing</key><description>Simple Text Greeting</description></value><value sequence_index="1"><id>1733</id><name>Howdy!</name><lang>en</lang><coords>39.648316,-77.719232</coords><read_token>1</read_token><write_token>11</write_token><last_access>1970-01-02 00:00:00</last_access><writeable>1</writeable><latitude>39.648316</latitude><longitude>-77.719232</longitude><payload_type>text/plain;base64</payload_type><payload>SEVMTE8gV09STEQ=</payload><key>basalt-test-0171: Another World</key><description>Simple Text Greeting</description></value><value sequence_index="2"><id>1734</id><name>Howdy!</name><lang>en</lang><coords>39.746352,-75.551615</coords><read_token>1</read_token><write_token>11</write_token><last_access>1970-01-02 00:00:00</last_access><writeable>1</writeable><latitude>39.746352</latitude><longitude>-75.551615</longitude><payload_type>text/plain;base64</payload_type><payload>SEVMTE8gV09STEQ=</payload><key>basalt-test-0171: Top Shot</key><description>Simple Text Greeting</description></value></things>';
+    
+    test_header($title, $method, $uri, $expected_result_code);
+
+    $st1 = microtime(true);
+    $result = clean_last_access_xml(call_REST_API($method, $uri, $data, $api_key, $result_code));
+
+    if ($result_code != $expected_result_code) {
+        test_result_bad($result_code, $result, $st1, $expected_result);
+        log_entry(false, 133, $title);
+    } else {
+        test_result_good($result_code, $result, $st1, $expected_result, false);
+        log_entry(true, 133, $title);
     }
 
     call_REST_API('GET', __SERVER_URI__.'/logout', NULL, $api_key, $result_code);
