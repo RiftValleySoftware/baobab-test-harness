af21f590534b36b5cd3b2d94561b39c7ef098a72
Chris Marshall <chris@riftvalleysoftware.com>
2021-02-19 08:06:53 -0500

    Duh. It was the SQL schema.

--- sql/things_tests_2_security_pgsql.sql
+++ sql/things_tests_2_security_pgsql.sql
@@ -11,26 +11,27 @@ CREATE TABLE co_security_nodes (
   write_security_id BIGINT DEFAULT NULL,
   object_name VARCHAR(255) DEFAULT NULL,
   access_class_context VARCHAR(4095) DEFAULT NULL,
-  ids VARCHAR(4095) DEFAULT NULL
+  ids VARCHAR(4095) DEFAULT NULL,
+  personal_ids VARCHAR(4095) DEFAULT NULL
 );
 
-INSERT INTO co_security_nodes (api_key, login_id, access_class, last_access, read_security_id, write_security_id, object_name, access_class_context, ids) VALUES
-(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL),
-(NULL, 'admin', 'CO_Security_Login', '1970-01-01 00:00:00', 2, 2, 'God Admin Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:4:\"JUNK\";}', NULL),
-(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL),
-(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL),
-(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL),
-(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL),
-(NULL, 'MDAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 7, 7, 'Maryland Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'VAAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 8, 8, 'Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'DCAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 9, 9, 'Washington DC Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'WVAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 10, 10, 'West Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'DEAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 11, 11, 'Delaware Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'MainAdmin', 'CO_Login_Manager', '1970-01-01 00:00:00', 12, 12, 'Main Admin Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '7,8,9,10,11'),
-(NULL, 'Dilbert', 'CO_Cobra_Login', '1970-01-01 00:00:00', 13, 13, 'Dilbert Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'Wally', 'CO_Cobra_Login', '1970-01-01 00:00:00', 14, 14, 'Wally Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'Ted', 'CO_Cobra_Login', '1970-01-01 00:00:00', 15, 15, 'Ted Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'Alice', 'CO_Cobra_Login', '1970-01-01 00:00:00', 16, 16, 'Alice Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'Tina', 'CO_Cobra_Login', '1970-01-01 00:00:00', 17, 17, 'Tina Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'PHB', 'CO_Login_Manager', '1970-01-01 00:00:00', 18, 18, 'Pointy-Haired Boss login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '13,14,15,16,17'),
-(NULL, 'MeLeet', 'CO_Security_Login', '1970-01-01 00:00:00', 19, 19, 'Elbonian Hacker', 'a:2:{s:4:"lang";s:2:"eb";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '2');
+INSERT INTO co_security_nodes (api_key, login_id, access_class, last_access, read_security_id, write_security_id, object_name, access_class_context, ids, personal_ids) VALUES
+(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL, NULL),
+(NULL, 'admin', 'CO_Security_Login', '1970-01-01 00:00:00', 2, 2, 'God Admin Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:4:\"JUNK\";}', NULL, NULL),
+(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL, NULL),
+(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL, NULL),
+(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL, NULL),
+(NULL, NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL, NULL),
+(NULL, 'MDAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 7, 7, 'Maryland Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'VAAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 8, 8, 'Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'DCAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 9, 9, 'Washington DC Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'WVAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 10, 10, 'West Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'DEAdmin', 'CO_Security_Login', '1970-01-01 00:00:00', 11, 11, 'Delaware Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'MainAdmin', 'CO_Login_Manager', '1970-01-01 00:00:00', 12, 12, 'Main Admin Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '7,8,9,10,11', NULL),
+(NULL, 'Dilbert', 'CO_Cobra_Login', '1970-01-01 00:00:00', 13, 13, 'Dilbert Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'Wally', 'CO_Cobra_Login', '1970-01-01 00:00:00', 14, 14, 'Wally Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'Ted', 'CO_Cobra_Login', '1970-01-01 00:00:00', 15, 15, 'Ted Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'Alice', 'CO_Cobra_Login', '1970-01-01 00:00:00', 16, 16, 'Alice Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'Tina', 'CO_Cobra_Login', '1970-01-01 00:00:00', 17, 17, 'Tina Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'PHB', 'CO_Login_Manager', '1970-01-01 00:00:00', 18, 18, 'Pointy-Haired Boss login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '13,14,15,16,17', NULL),
+(NULL, 'MeLeet', 'CO_Security_Login', '1970-01-01 00:00:00', 19, 19, 'Elbonian Hacker', 'a:2:{s:4:"lang";s:2:"eb";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '2', NULL);
